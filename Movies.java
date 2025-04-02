73f32e07799d1fb3d7d7c9668eb21000f6274a4a
Ana-Ioana Popescu <160779920+AnaIoanaPopescu@users.noreply.github.com>
2025-04-02 17:54:47 +0300

    Add files via upload

---
+++ Movies.java
@@ -0,0 +1,25 @@
+package Models;
+
+public class Movies {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
+
